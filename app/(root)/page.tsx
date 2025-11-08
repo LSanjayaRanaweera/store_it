cf175de0b4d894d5c3f5698340640039993dc65b
Lulitha Sanjaya Ranaweera <lsanjayaranaweera@gmail.com>
2025-11-08 16:49:15 -0500

    General layout implemented for main page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -16,4 +16,10 @@ app/layout.tsx -> for global layout and <body> structure
 app/head.tsx -> for <head> content like title, meta, links etc.
 ------------------------------------------------------------------------------------------------------------------------
 app/globals.css == global stylesheet the styles define in here will be applied across the entire application
+-----------------------------------------------------------------------------------------------------------------------
+NOTE: After working on (auth) directory, AuthForm.tsx etc., created a new folder >> app/(root)
+In the (root) directory, create a new layout.tsx that will implement the common styles for all directory implementations.
+Next, moved this file from app/page.tsx to (root) folder >> app/(root)/page.tsx
+The root view would have a SideBar, a MobileNavigation and a Header
+Created new files for them in 'components' folder >> Sidebar.tsx, MobileNavigation.tsx, Header.tsx
 */
