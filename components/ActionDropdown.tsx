69edb7d214d7ab4d8412112b30023f24b7ccfd25
Lulitha Sanjaya Ranaweera <lsanjayaranaweera@gmail.com>
2025-11-13 12:27:17 -0500

    Build rename feature in ActionDropdown

--- components/ActionDropdown.tsx
+++ components/ActionDropdown.tsx
@@ -1,7 +1,159 @@
-import React from "react";
-import { Models } from "node-appwrite";
+"use client"; // Especially if we are using useState hook, this needs to be run ONLY on client side
 
-const ActionDropdown = (file: Models.Document) => {
-  return <div>ActionDropdown</div>;
+import {
+  Dialog,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { useState } from "react";
+import Image from "next/image";
+import { Models } from "node-appwrite";
+import { actionsDropdownItems } from "@/constants";
+import Link from "next/link";
+import { constructDownloadUrl } from "@/lib/utils";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+
+const ActionDropdown = ({ file }: { file: Models.Document }) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isDropDownOpen, setIsDropDownOpen] = useState(false);
+  const [action, setAction] = useState<ActionType | null>(null);
+  const [name, setName] = useState(file.name);
+  const [isLoading, setIsLoading] = useState(false);
+
+  // helper 1- if we cancel the action
+  const closeAllModals = () => {
+    setIsModalOpen(false);
+    setIsDropDownOpen(false);
+    setAction(null);
+    setName(file.name);
+    // setEmail[]
+  };
+  // helper 2 - if we don't cancel the action
+  const handleAction = async () => {};
+  
+  // what does this helper function do?
+  const renderDialogContent = () => {
+    if (!action) return null;
+    const { value, label } = action;
+
+    return (
+      <DialogContent className="shad-dialog button">
+        <DialogHeader className="flex flex-col gap-3">
+          <DialogTitle className="text-center text-light-100">
+            {label}
+          </DialogTitle>
+          {value === "rename" && (
+            <Input
+              type="text"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+          )}
+        </DialogHeader>
+        {["rename", "delete", "share"].includes(value) && (
+          <DialogFooter className="flex flex-col gap-3 md:flex-row">
+            <Button onClick={closeAllModals} className="modal-cancel-button">
+              Cancel
+            </Button>
+            <Button onClick={handleAction} className="modal-submit-button">
+              <p className="capitalize">{value}</p>
+              {isLoading && (
+                <Image
+                  src="/assets/icon/loader.svg"
+                  alt="loader"
+                  height={24}
+                  width={24}
+                  className="animate-spin"
+                />
+              )}
+            </Button>
+          </DialogFooter>
+        )}
+      </DialogContent>
+    );
+  };
+
+  return (
+    // Go over the attributes of <Dialog /> and <DropdownMenu /> (from shadcn)  and their assignments to useState()
+    <Dialog open={isModalOpen} onOpenChange={setIsModalOpen}>
+      <DropdownMenu open={isDropDownOpen} onOpenChange={setIsDropDownOpen}>
+        <DropdownMenuTrigger className="shad-no-focus">
+          <Image
+            src="/assets/icons/dots.svg"
+            alt="dots"
+            width={34}
+            height={34}
+          />
+        </DropdownMenuTrigger>
+        <DropdownMenuContent>
+          <DropdownMenuLabel className="max-w-[200px] truncate">
+            {file.name}
+          </DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          {actionsDropdownItems.map((actionItem) => (
+            <DropdownMenuItem
+              key={actionItem.value}
+              className="shad-dropdown-item"
+              onClick={() => {
+                setAction(actionItem);
+
+                if (
+                  ["rename", "details", "share", "delete"].includes(
+                    actionItem.value,
+                  )
+                ) {
+                  setIsModalOpen(true);
+                }
+              }}
+            >
+              {actionItem.value === "download" ? (
+                <Link
+                  href={constructDownloadUrl(file.bucketFileId)} // Important to have correct construct(fn) for href
+                  download={file.name}
+                  className="flex items-center gap-2"
+                >
+                  <Image
+                    src={actionItem.icon}
+                    alt={actionItem.label}
+                    width={30}
+                    height={30}
+                  />
+                  {actionItem.label}
+                </Link>
+              ) : (
+                <div className="flex items-center gap-2">
+                  <Image
+                    src={actionItem.icon}
+                    alt={actionItem.label}
+                    width={30}
+                    height={30}
+                  />
+                  {actionItem.label}
+                </div>
+              )}
+            </DropdownMenuItem>
+          ))}
+        </DropdownMenuContent>
+      </DropdownMenu>
+      {renderDialogContent()}
+    </Dialog>
+  );
 };
 export default ActionDropdown;
+/*
+ In the parameters ({file}: {file: Models.Document}),
+    the {file} === PROP
+    the {file: Models.Document} == data type
+
+*/
