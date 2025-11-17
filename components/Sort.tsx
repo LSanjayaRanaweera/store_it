1ad55098cf273e36fd0f95363ba6ebbf60bd5b0e
Lulitha Sanjaya Ranaweera <lsanjayaranaweera@gmail.com>
2025-11-17 12:17:18 -0500

    Implemented sort feature

--- components/Sort.tsx
+++ components/Sort.tsx
@@ -1,9 +1,41 @@
-import React from 'react'
+"use client"; // Anytime using useState, next/navigation (useRouter) is an indication it is going to run on client
+
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { usePathname, useRouter } from "next/navigation";
+import { sortTypes } from "@/constants";
 
 const Sort = () => {
-  return (
-    <div>Sort</div>
-  )
-}
+  const path = usePathname();
+  const router = useRouter(); // Routing functionality with useRouter
 
-export default Sort
+  const handleSort = (value: string) => {
+    router.push(`${path}?sort=${value}`); // Go over
+  };
+
+  return (
+    <Select onValueChange={handleSort} defaultValue={sortTypes[0].value}>
+      <SelectTrigger className="sort-select">
+        <SelectValue placeholder={sortTypes[0].value} />
+      </SelectTrigger>
+      <SelectContent className="sort-select-content">
+        {sortTypes.map((sort) => (
+          <SelectItem
+            key={sort.label}
+            value={sort.value}
+            className="shad-select-item"
+          >
+            {sort.label}
+          </SelectItem>
+        ))}
+      </SelectContent>
+    </Select>
+  );
+};
+
+export default Sort;
